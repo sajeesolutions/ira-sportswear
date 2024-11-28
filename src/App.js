27f65f454f70fb3857eefa7cecaefad8b559d58a
Shyamnarayananj <jshyamnarayanan@gmail.com>
2024-11-28 15:24:08 +0530

    Added Contact Page using: react-hook-form(^7.53.2), @hookform/resolvers(^3.9.1), zod(^3.23.8)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 //import './extra.css';
 import Header from './controllers/iraheader.js';  
+import ContactUs from './views/contactpage.js';
 
 
 function App() {
   return (
+    <>
     <div className="text-center rounded-lg h-100">
      <Header />
     </div>
+    <ContactUs />
+    </>
   );
 }
 
